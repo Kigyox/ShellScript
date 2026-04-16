3cd4047254973485a9b58ba2cd1d497c3ddcbe9b
kigyox <ifernande@inscamidemar.cat>
2026-04-16 21:21:10 +0200

    Afegir comentaris a scripts de creacio d'usuari

--- crear_usuari.sh
+++ crear_usuari.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+#Preguntar a l'usuari el nom del usuari que vol afegir
 echo "Quin és el nom de l'usuari que vols afegir: "
 read usuari
 
+#Preguntem el nom complet
 echo "Nom complet de l'usuari: "
 read nom
 
+#Despres el grup
 echo "A quin grup vols afegir aquest usuari: "
 read grup
 
+#Fem un grep al arxiu /etc/group per comprovar si el grup existeix si es aixi es crear normal el usuari
 if grep -q "$grup" /etc/group; then
     echo "El grup ja existeix, creant l'usuari en aquest grup"
     useradd -m -c "$nom" -g "$grup" "$usuari"
 else
+#No existeix es creara el grup i despres s'afegira el suuari
     echo "El grup no existeix, creant el grup..."
     groupadd "$grup"
     echo "Afegint l'usuari al grup..."
